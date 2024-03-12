82dbfe87e10ec1382b79c2967eabfc49ba9749a2
jomorenoo <jomorenoo@unal.edu.co>
2024-03-12 09:57:47 -0500

    intento2

--- preguntas.py
+++ preguntas.py
@@ -136,8 +136,8 @@ def pregunta_08():
     39   39   E    5  1998-01-26    44
 
     """
-    column_sum = tbl0["_c0"] + tbl0["_c2"]
-    return column_sum
+    tbl0["suma"] = tbl0["_c0"] + tbl0["_c2"]
+    return tbl0
 
 
 def pregunta_09():
